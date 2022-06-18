501dc398eb038a8dace2767029a3d97974ac9097
Marcus <marcuscchan@gmail.com>
2022-06-18 12:14:39 -0700

    init

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt update
+
+sudo apt install git-all -y
+git config --global user.name "Marcus"
+git config --global user.email marcuscchan@gmail.com
+git config --global init.defaultBranch main
+
+sudo apt install i3
+
+sudo snap install obs-studio
+sudo snap install discord
+sudo snap install vlc
+sudo snap install spotify
+sudo snap install code --classic
